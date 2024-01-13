eb592053e65ab70ef53dab5242430a6aa6f90c97
kmahendra999 <k.mahendra999@hotmail.com>
2024-01-14 01:29:53 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+// Get the server's IP address
+$serverIP = $_SERVER['SERVER_ADDR'];
+
+// Print the server's IP address
+echo "Server IP Address: " . $serverIP . "<br>";
+
+// Attempt to get additional network information (may not work on all servers)
+if (function_exists('shell_exec')) {
+    $networkInfo = shell_exec('ifconfig');
+    echo "<pre>Network Info:\n" . $networkInfo . "</pre>";
+} else {
+    echo "Network information retrieval is not supported on this server.";
+}
+
+?>
